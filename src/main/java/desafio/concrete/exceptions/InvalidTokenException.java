fa21926d4624d1a64bcc42be22e56aab39df3a51
rubenssantoTI <rubensgomes34@gmail.com>
2017-03-26 22:39:45 -0300

    Comtando arquivo de Token

--- src/main/java/desafio/concrete/exceptions/InvalidTokenException.java
+++ src/main/java/desafio/concrete/exceptions/InvalidTokenException.java
@@ -5,7 +5,7 @@ import static java.lang.String.format;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value=HttpStatus.IM_USED, reason="Não autorizado")
+@ResponseStatus(value=HttpStatus.UNAUTHORIZED, reason="Não autorizado")
 public class InvalidTokenException extends RuntimeException{
 	private static final long serialVersionUID = -6999475804487963563L;
 	
